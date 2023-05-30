0533ae8771f2dfbcb4c32c0b2ccbc085925450b0
StartAt24 <779058143@qq.com>
2023-05-30 23:56:48 +0800

    Version 1.0 of ArrayList

--- basic_data_structure/ArrayList.cpp
+++ basic_data_structure/ArrayList.cpp
@@ -14,17 +14,47 @@ class ArrayList{
         if (m_capacity == m_size) {
             resize(m_capacity*2);
         }
-        m_pArr[size] = e;
+        m_pArr[m_size] = e;
         m_size++;
     }
 
     bool add(int idx, E e) {
         if (!isPositionIndex(idx))
-            return false; 
-        
+            return false;
+        // data[idx..] move to data[idx+1 ..]
+        int elementCountToMove = m_size -idx;
+        if (elementCountToMove == 0) {
+            m_pArr[idx] = e;
+            m_size++;
+            return true;
+        }
+
+        for (int i=m_size; i>idx; i--) {
+            m_pArr[i] = m_pArr[i-1];
+        }
+
+        m_pArr[idx] = e;
+        m_size++;
+        return true;
     }
 
-    void removeLast(int index) {
+    bool get(int idx, E& out) {
+        if (!isElementIndex(idx)) {
+            return false;
+        }
+        out = m_pArr[idx];
+        return true;
+    }
+
+    bool set(int idx, E in) {
+        if (!isElementIndex(idx)) {
+            return false;
+        }
+        m_pArr[idx] = in;
+        return true;
+    }
+
+    void removeLast() {
         if (isEmpty())
             return;
         m_size--;
@@ -32,6 +62,27 @@ class ArrayList{
         if (m_size < m_capacity/4) {
             resize(m_capacity/2);
         }
+    }
+
+    void removeIndex(int idx) {
+        if (!isElementIndex(idx))
+            return;
+        // clear element
+        m_pArr[idx].~E();
+        // move other elements
+        if (idx == m_size-1) {
+            m_size--;
+            return;
+        }
+
+        for (int i=idx; i<m_size-1; i++) {
+            m_pArr[idx] = m_pArr[idx+1];
+        }
+        // clear last elemet;
+        m_pArr[m_size-1].~E();
+
+        m_size--;
+        return;
     }
     
 private:
