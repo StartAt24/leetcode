7c63b32711b852b9109bfff7240956445adc56db
StartAt24 <779058143@qq.com>
2023-08-15 00:30:46 +0800

    ugly number, merge sorted list

--- basic_data_structure/list/264_ugly_number.cpp
+++ basic_data_structure/list/264_ugly_number.cpp
@@ -2,37 +2,36 @@
 
 class Solution {
     int nthUglyNumber(int n) {
-        // so the numer is combination of 2*2*... or 2*3*x or 2*5*
-        // three list
-        // 2, 2, 2, 2, 2
-        // 3, 3, 3, 3, 3
-        // 5, 5, 5, 5, 5
-        // put into a 
-        int idxTwo = 0, idxThree = 0, idxFive=0;
+        // three lists
+        // 1*2, 2*2, 3*2, 4*2, 5*2, 6*2, 8*2...
+        // 1*3, 2*3, 3*3, 4*3, 5*3, 6*3, 8*3...
+        // 1*5, 2*5, 3*5, 4*5, 5*5, 6*5, 8*5...
+        // merge those three list.
+        // !!!remember to use the array that generated during the process.
+        int p2 = 0, p3 = 0, p5=0;
+        int next2 = 0, next3 = 0, next5 = 0;
         vector<int> ret;
         ret.reserve(n);
         ret.push_back(1);
-        int basic = 1;
-        int p_2 = 0, p_3 = 0, p_5 = 0;
 
-        // we need n_th number
-        for (int i = 0; i <n; i++){
-            // get one from each list and compare them; 
-            // get p_2 element from the 2-list;
-            int val = 1;
-            
-            // get one extra val from the list and compare them
+        for (int i = 1; i < n; i++) {
+            next2 = ret[p2]*2;
+            next3 = ret[p3]*3;
+            next5 = ret[p5]*5;
 
+            ret[i] = std::min({next2, next3, next5});
+            if (ret[i] == next2){
+                p2++;
+            }
 
+            if (ret[i] == next3){
+                p3++;
+            }
 
-            int val3 = 1;
-            for (int j = 0; j <= p_3; j++)
-                val3*=3;
-            // get p_3 element from the 3-list; 
-            // get p_5 element from the 5-list;
-            int val3 = basic*5;
-
+            if (ret[i] == next5){
+                p5++;
+            }
         }
-
+        return ret[n-1];
     }
 };
