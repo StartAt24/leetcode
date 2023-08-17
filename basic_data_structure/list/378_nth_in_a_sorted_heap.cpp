7a15377926d773abba9d140d846a2931b79c9c0b
StartAt24 <779058143@qq.com>
2023-08-18 00:36:07 +0800

    nth min val

--- basic_data_structure/list/378_nth_in_a_sorted_heap.cpp
+++ basic_data_structure/list/378_nth_in_a_sorted_heap.cpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
+#include <limits>
 
 // 即每個内部的数组 按从小到大排序
 // 每个数组的相同位置的元素按照从小到大排序
@@ -13,26 +14,60 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         // 合并N个有序链表
-        vector<int> listIdx(0, matrix.size());
-        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        int listCount = matrix.size();
+        vector<int> listIdx(listCount);
+        // max heap
+        std::priority_queue<int, std::vector<int>, std::less<int>> maxHeap;
         for(int i = 0; i < k; i++) {
             // loop all the list;
-            int minVal = std::numeric_limit<int>::max();
-            for (int idx = 0; idx < matrix.size(); idx++) {
-                int val = matrix[idx][listIdx[idx]];
-                if(val<minVal)
+            int minVal = std::numeric_limits<int>::max();
+
+            for (int idx = 0; idx < listCount; idx++) {
+                const auto& currentList = matrix[idx];
+                int pointer = listIdx[idx];
+                if (pointer >= matrix[0].size())
+                    continue;
+                int val = currentList[pointer];
+                if( val < minVal)
                     minVal = val;
             }
 
-            for (int j = 0; j < matrix.size(); j++) {
+            for (int j = 0; j < listCount; j++) {
                 int val = matrix[j][listIdx[j]];
-                if (val == minVal)
+                if (val == minVal) {
                     listIdx[j]++;
+                    break;
+                }
             }
 
-            minHeap.push(minVal);
+            maxHeap.push(minVal);
         }
 
-        return minHeap.top();
+        return maxHeap.top();
     }
 };
+
+int main() {
+    vector<vector<int>> matrix(2);
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+
+    vector<int> b;
+    b.push_back(1);
+    b.push_back(3);
+    
+    vector<int> c;
+    c.push_back(12);
+    c.push_back(13);
+    c.push_back(15);
+
+    matrix[0] = a;
+    matrix[1] = b;
+    // matrix[2] = c;
+
+    int k = 4;
+    Solution s;
+    int val = s.kthSmallest(matrix, k);
+    return 0;
+}
