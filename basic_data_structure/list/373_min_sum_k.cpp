a834a1f9b0416b81884b15a3b6666bf5920dfebb
StartAt24 <779058143@qq.com>
2023-08-20 00:00:06 +0800

    min_sum

---
+++ basic_data_structure/list/373_min_sum_k.cpp
@@ -0,0 +1,18 @@
+#include "common.hpp"
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int p1 = 0;
+        int p2 = 0;
+        // 1, 7, 11
+        // 2, 4, 6
+
+        vector<vector<int>> res;
+        for (int i = 0; i < k; i ++) {
+            
+        }
+        return res; 
+    }
+private:
+
+};
