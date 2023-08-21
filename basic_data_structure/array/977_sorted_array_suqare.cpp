d3d904e4b17939f3c001efc84f484809f724f369
StartAt24 <779058143@qq.com>
2023-08-21 23:34:39 +0800

    977 sotred suqare

--- basic_data_structure/array/977_sorted_array_suqare.cpp
+++ basic_data_structure/array/977_sorted_array_suqare.cpp
@@ -2,6 +2,25 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+        // two pointer, one from begin another from the end?
+        // and sort the nums from the last one;
+        int l = 0, r = nums.size()-1;
+        std::vector<int> res;
+        int size = nums.size();
+        res.resize(size);
+
+        while (l<=r){
+            int left = nums[l]*nums[l];
+            int right = nums[r]*nums[r];
+
+            int max = std::max(left, right);
+            if (max == left) {
+                l++;
+            } else {
+                r--;
+            }
+            res[--size] = max;
+        }
+        return res;
     }
 };
