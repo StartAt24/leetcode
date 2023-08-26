92d180bb40bdb10d05b727f7fc55172b433530c9
StartAt24 <ihave6questions@gmail.com>
2023-08-27 00:07:32 +0800

    finish

--- basic_data_structure/array/59_apiral_matrix_2.cpp
+++ basic_data_structure/array/59_apiral_matrix_2.cpp
@@ -2,6 +2,45 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
+        // reserve size
+        vector<vector<int>> res(n);
+        for (auto& line: res ){
+            line.resize(n);
+        }
+        int top_limit = 0, right_limit= n-1, bottom_limit=n-1, left_limit=0;
+        for (int i = 1; i <= n; i++){
+            // top
+            if (top_limit <= bottom_limit) {
+                for (int x = left_limit; x <= right_limit; x++){
+                    res[top_limit][x] = i;
+                }
+                top_limit++;
+            }
 
+            // right
+            if (left_limit <= right_limit) {
+                for (int y = top_limit; y<= bottom_limit; y++){
+                    res[y][right_limit] = i;
+                }
+                right_limit--
+            }
+
+            // bottom
+            if (top_limit <= bottom_limit) {
+                for (int x = right_limit; x>=left_limit; x--) {
+                    res[bottom_limit][x] = i;
+                }
+                bottom_limit--;
+            }
+
+            // left
+            if (left_limit <= right_limit) {
+                for (int y = bottom_limit; y>= top_limit; y--){
+                    res[y][left_limit] = i;
+                }
+                left_limit++;
+            }
+        }
+        return res;
     }
 };
