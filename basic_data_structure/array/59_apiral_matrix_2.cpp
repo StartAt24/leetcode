2c551ef886034146162fae6f7f675de8b60beada
StartAt24 <ihave6questions@gmail.com>
2023-08-27 00:11:44 +0800

    fix error

--- basic_data_structure/array/59_apiral_matrix_2.cpp
+++ basic_data_structure/array/59_apiral_matrix_2.cpp
@@ -8,11 +8,12 @@ public:
             line.resize(n);
         }
         int top_limit = 0, right_limit= n-1, bottom_limit=n-1, left_limit=0;
-        for (int i = 1; i <= n; i++){
+        int num = 1;
+        while(num <= n*n){
             // top
             if (top_limit <= bottom_limit) {
                 for (int x = left_limit; x <= right_limit; x++){
-                    res[top_limit][x] = i;
+                    res[top_limit][x] = num++;
                 }
                 top_limit++;
             }
@@ -20,15 +21,15 @@ public:
             // right
             if (left_limit <= right_limit) {
                 for (int y = top_limit; y<= bottom_limit; y++){
-                    res[y][right_limit] = i;
+                    res[y][right_limit] = num++;
                 }
-                right_limit--
+                right_limit--;
             }
 
             // bottom
             if (top_limit <= bottom_limit) {
                 for (int x = right_limit; x>=left_limit; x--) {
-                    res[bottom_limit][x] = i;
+                    res[bottom_limit][x] = num++;
                 }
                 bottom_limit--;
             }
@@ -36,7 +37,7 @@ public:
             // left
             if (left_limit <= right_limit) {
                 for (int y = bottom_limit; y>= top_limit; y--){
-                    res[y][left_limit] = i;
+                    res[y][left_limit] = num++;
                 }
                 left_limit++;
             }
