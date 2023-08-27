bd5d29885703a4fdbff14bf21a4b7d1569203571
StartAt24 <779058143@qq.com>
2023-08-27 16:40:08 +0800

    accumute sum

--- basic_data_structure/array/303_range_sum.cpp
+++ basic_data_structure/array/303_range_sum.cpp
@@ -15,3 +15,23 @@ public:
 private:
     std::vector<int> _nums;
 };
+
+class NumArray2 {
+public:
+    NumArray2(vector<int>& nums) {
+        // calc the accu value when initialize
+        // 0 as the first index to calc easlier.
+        _nums.resize(nums.size() + 1);
+        _nums[0] = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            _nums[i+1] = _nums[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return _nums[right+1] - _nums[left];
+    }
+private:
+    std::vector<int> _nums;
+};
