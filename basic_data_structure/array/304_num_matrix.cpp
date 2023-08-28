e67f346d309c2fd50afda87af749a73148d00a4e
StartAt24 <ihave6questions@gmail.com>
2023-08-28 23:06:41 +0800

    basic consutruction

---
+++ basic_data_structure/array/304_num_matrix.cpp
@@ -0,0 +1,11 @@
+#include "common.hpp"
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+
+    }
+};
