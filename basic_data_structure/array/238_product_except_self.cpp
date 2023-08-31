e05343247c5e2868f1f7fdae2f243c3002a282b6
StartAt24 <ihave6questions@gmail.com>
2023-08-31 23:25:05 +0800

    fix bug

--- basic_data_structure/array/238_product_except_self.cpp
+++ basic_data_structure/array/238_product_except_self.cpp
@@ -17,10 +17,12 @@ public:
             after_product_vec[i] = after_product_vec[i+1] * nums[i];
         }
 
-        for (int i = 0; i < nums.size(); i++) {
+        res[0] = after_product_vec[1];
+        res[n-1] = pre_product_vec[n-2];
+        for (int i = 1; i < nums.size()-1; i++) {
             res[i] = pre_product_vec[i-1]*after_product_vec[i+1];
         }
-
+        
         return res;
     }
 };
