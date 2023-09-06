072228e9fe4681eea00248280e5cad6f951869c6
StartAt24 <779058143@qq.com>
2023-09-06 23:31:09 +0800

    I am stupid

---
+++ basic_data_structure/array/1658_min_operations.cpp
@@ -0,0 +1,10 @@
+#include "common.hpp"
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        // 反过来思考，寻找最长连续子数组
+        // 使得 最长连续子数组的和为 nums.sum() - x;
+        int n = nums.size();
+        for(int i = 0;)
+    }
+};
