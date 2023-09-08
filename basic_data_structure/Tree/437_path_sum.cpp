98fdb2cfca5ed6d86a3c39eb858a7e4b28ce8b9c
StartAt24 <779058143@qq.com>
2023-09-08 22:14:20 +0800

    tree begin

---
+++ basic_data_structure/Tree/437_path_sum.cpp
@@ -0,0 +1,13 @@
+#include "common.hpp"
+
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        // loop in this tree
+    }
+
+    int helper() {
+        
+    }
+
+};
--- basic_data_structure/list/common.hpp
+++ basic_data_structure/list/common.hpp
@@ -10,4 +10,4 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+;
