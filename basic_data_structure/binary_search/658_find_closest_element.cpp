fa62547f428097bbe3de88c76c51574c025e8875
StartAt24 <779058143@qq.com>
2023-09-23 22:02:05 +0800

    pre commit

---
+++ basic_data_structure/binary_search/658_find_closest_element.cpp
@@ -0,0 +1,18 @@
+#include "common.hpp"
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // binary search the indx;
+        
+    }
+
+private:
+    int findMostLeftIndex(vector<int>& arr, int target) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            int val = 
+        }
+    }
+};
