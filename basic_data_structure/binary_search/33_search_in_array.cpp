b83e0c93346b020256e1b0f0a1e593001dd58032
StartAt24 <779058143@qq.com>
2023-09-28 00:10:57 +0800

    33 search in array, how do they come up with all those questions?

--- basic_data_structure/binary_search/33_search_in_array.cpp
+++ basic_data_structure/binary_search/33_search_in_array.cpp
@@ -3,6 +3,38 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // 相当于把一个连续的数组截成了两个
+        // 且两端都是连续的
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = nums[mid];
+            // 确定 mid当前的位置是在哪里
+            if (mid_val == target)
+                return mid;
 
+            if (mid_val >= nums[left]) {
+                // mid 在左侧断崖上
+                // 则 [left, mid] 为单调递增
+                if (nums[left] <= target && target < mid_val) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+
+            } else {
+                // mid 在右侧断崖上
+                // 原因是右侧断崖上的所有数字都比左侧小
+                // [mid, right] 为单调递增
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 };
