abb4b10c65933a60ab4d2d0ee8c55820d52e9c7e
StartAt24 <779058143@qq.com>
2023-09-28 23:38:40 +0800

    use diagram to help understanding the question

--- basic_data_structure/binary_search/33_search_in_array.cpp
+++ basic_data_structure/binary_search/33_search_in_array.cpp
@@ -7,6 +7,13 @@ public:
         // 且两端都是连续的
         int left = 0, right = nums.size() - 1;
         while(left <= right) {
+            // 因为存在水平的区域，所以要先把这部分剔除掉，这样逻辑就和之前一样了。
+            while (left < right && nums[left] == nums[left+1])
+                left++;
+            
+            while (left < right && nums[right] == nums[right-1])
+                right--;
+
             int mid = left + (right - left) / 2;
             int mid_val = nums[mid];
             // 确定 mid当前的位置是在哪里
