243a1eb1b1d79c9a574c50246459e93a6611c0fd
StartAt24 <779058143@qq.com>
2023-10-06 23:35:40 +0800

    finish 143 resort

--- basic_data_structure/stack/143_resort_list.cpp
+++ basic_data_structure/stack/143_resort_list.cpp
@@ -11,18 +11,22 @@ public:
             s.push(p);
             p = p->next;
         }
-        // 4个的话 需要循环两次
-        // 3个的话 需要循环1一次？
-        ListNode* front = head;
-        ListNode* back = s.top();
-        ListNode dummyHead;
-        ListNode* pDummy = &dummyHead;
 
-        int size = s.size();
-        while() {
-
+        // reset p;
+        p = head;
+        while(p) {
+            ListNode* lastNode = s.top();
+            s.pop();
+            ListNode* next = p->next;
+            // 找到重合的地方
+            // 链表为奇数或者偶数的时候均适用
+            if (lastNode == next || lastNode->next == next) {
+                lastNode->next = nullptr;
+                break;                
+            }
+            p->next = lastNode;
+            lastNode->next = next;
+            p = next;
         }
-
-        pDummy->next = nullptr;
     }
 };
