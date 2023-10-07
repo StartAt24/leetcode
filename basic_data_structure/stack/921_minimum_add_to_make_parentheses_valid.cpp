2affbce5cc62da9a2913da80d1f6bcad55fe977e
StartAt24 <779058143@qq.com>
2023-10-07 23:24:18 +0800

    921 make parentheses valid

--- basic_data_structure/stack/921_minimum_add_to_make_parentheses_valid.cpp
+++ basic_data_structure/stack/921_minimum_add_to_make_parentheses_valid.cpp
@@ -1,2 +1,21 @@
 #include <stack>
 using namespace std;
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> stk;
+        for(auto c: s) {
+            if(c == ')') {
+                if (!stk.empty() && stk.top() == '(')
+                    stk.pop();
+                else
+                    stk.push(c);
+            }
+            else {
+                stk.push(c);
+            }
+        }
+        return stk.size();
+    }
+};
