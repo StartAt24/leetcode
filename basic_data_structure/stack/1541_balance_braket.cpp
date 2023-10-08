e43ec7ab1ea89eae74bec2b81d50883fad88a066
StartAt24 <779058143@qq.com>
2023-10-08 23:50:01 +0800

    1541 balance a parathesis

--- basic_data_structure/stack/1541_balance_braket.cpp
+++ basic_data_structure/stack/1541_balance_braket.cpp
@@ -5,19 +5,31 @@ using namespace std;
 class Solution {
 public:
     int minInsertions(string s) {
-        stack<char> stk;  
-        int cnt = 0;
-        for(int i = 0; i < s.size() - 1; i++) {
-            if(s[i] == ')' && s[i+1] == ')') {
-                if (!stk.empty() && stk.top() == '(') {
-                    stk.pop();
-                } else {
-                    stk.push(s[i]);
+        // need 记录需要有括号的数量
+        int res = 0, need = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                need += 2;
+                if (need % 2 == 1) {
+                    // 插入一个右边括号
+                    res++;
+                    // 对右括号的需求-1
+                    need--;
                 }
-            } else {
-                stk.push(s[i]);
+            }
+
+            if (s[i] == ')') {
+                need--;
+                // 说明 )太多了
+                if (need == -1) {
+                    // 需要一个左括号
+                    res++;
+                    // 同时重置 need 为 1 因为左括号一对二的原因，右括号又需要一个
+                    need = 1;
+                }
             }
         }
 
+        return res + need;
     }
 };
