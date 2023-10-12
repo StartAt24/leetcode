7fda9dba85bbd474ca747591b9edf76b3d5bac58
StartAt24 <779058143@qq.com>
2023-10-12 23:01:05 +0800

    388 longest file path

--- basic_data_structure/stack/388_longest_file_path.cpp
+++ basic_data_structure/stack/388_longest_file_path.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <algorithm>
 #include <stack>
+#include <vector>
 using namespace std;
 
 class Solution {
@@ -8,37 +9,51 @@ public:
     int lengthLongestPath(string input) {
         stack<string> stk;
         int maxLen = 0;
-        while(1) {
-            int idx = input.find_first_of('\n');
+        auto parts = SplitStr(input, '\n');
+        for (auto part : parts) {
             // split with \n so the string would look like this "\txxx\t\txxx"
-            if (idx != std::string::npos) {
-                // first string
-                string temp = input.substr(0, idx);
-                input = input.substr(idx+1);
-                int level = temp.find_last_of('\t') + 1;
+            // first string
+            int level = part.find_last_of('\t') + 1;
 
-                // 让栈只保留当前目录的父路径
-                while (level < stk.size()) {
-                    stk.pop();
+            // 让栈只保留当前目录的父路径
+            while (level < stk.size()) {
+                stk.pop();
+            }
+
+            // 当前目录或者文本入栈
+            stk.push(part);
+
+            if (part.find('.') != string::npos) {
+                stack<string> stk_cpy = stk;
+                int sum = 0;
+                while(!stk_cpy.empty()) {
+                    sum += stk_cpy.top().size();
+                    stk_cpy.pop();
                 }
+                // 加上所有的 '/'
+                sum += stk.size()-1;
+                maxLen = std::max(sum, maxLen);
+            }
 
-                // 当前目录或者文本入栈
-                stk.push(temp.substr(level));
-
-                if (temp.find('.') != string::npos) {
-                    stack<string> stk_cpy = stk;
-                    int sum = 0;
-                    while(!stk_cpy.empty()) {
-                        sum += stk_cpy.top().size();
-                        stk_cpy.pop();
-                    }
-
-                    // 加上所有的 '/'
-                    sum += stk.size()-1;
-                    maxLen = std::max(sum, maxLen);
+        }
+        return maxLen;
+    }
+private:
+    vector<string> SplitStr(string input, char c) {
+        vector<string> ret;
+        while(1) {
+            int pos = input.find_first_of(c);
+            if (pos != string::npos) {
+                ret.push_back(input.substr(0, pos));
+                input = input.substr(pos+1);
+            } else {
+                // 没有 \n了 可能是
+                if (!input.empty()) {
+                    ret.push_back(input);
                 }
-
+                break;
             }
         }
+        return ret;
     }
 };
