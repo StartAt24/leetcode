e8c9d2a81600f26f42147e4241fbe1b3bf692c23
StartAt24 <779058143@qq.com>
2023-10-15 00:56:09 +0800

    239 wrong answer

--- basic_data_structure/stack/239_sliding_window_maximum.cpp
+++ basic_data_structure/stack/239_sliding_window_maximum.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <stack>
+#include <queue>
 using namespace std;
 
 class Solution {
@@ -7,25 +8,39 @@ public:
     // 每次滑动之后应该想办法不全部比较一遍, 借助上次比较的结果
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> maxVec;
-        int firstMax = nums[0];
-        for(int i = 0; i < k; i++) {
-            firstMax = std::max(nums[i], firstMax);
+        queue<int> maxStk;
+
+        for (int i = 0; i <k-1; i++) {
+            if (maxStk.empty() || maxStk.back() <= nums[i]) {
+                maxStk.push(nums[i]);
+            } else {
+                maxStk.push(maxStk.back());
+            }
         }
 
-        maxVec.push_back(firstMax);
-        stack<int> maxStk;
-        maxStk.push(firstMax);
+        // 7,2,4
+        // k = 2;
+        // 7,7
 
-        int left = k, right = k;
-        int currentIdx = k;
-        
+
+        int left = 0, right = k-1;
         while (right < nums.size()) {
-            if (right - left >= k) {
-                
+            if (right - left == k) {
+                // need to pop left;
+                // and left is the biggest value;
+                while (!maxStk.empty() && maxStk.front() == nums[left])
+                    maxStk.pop();
+                left++;
             }
 
-            
-            currentIdx++;
+            if (maxStk.empty() || maxStk.back() < nums[right]) {
+                maxStk.push(nums[right]);
+            } else {
+                maxStk.push(maxStk.back());
+            }
+
+            right++;
+            maxVec.push_back(maxStk.back());
         }
         return maxVec;
     }
