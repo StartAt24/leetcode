87725742e7ef754ba4eb87ccca01d6f7f2413e88
StartAt24 <779058143@qq.com>
2023-10-16 23:54:55 +0800

    understand single increase list

--- basic_data_structure/stack/sorted_stack.cpp
+++ basic_data_structure/stack/sorted_stack.cpp
@@ -17,6 +17,10 @@ vector<int> nextGreaterElement(vector<int>& nums) {
     stack<int> sortedStk;
 
     for (int i = n-1; i >=0 ; i--) {
+        // 从后向前入栈，其实就是从前向后处理。
+        // 并且只保留大于当前元素的元素。
+        // 栈的作用就是先进去的后处理。并且从前向后处理会把所有等于当前元素的也全部去掉。
+        // 这样留下来的始终是index比较小的元素
         while(!sortedStk.empty() && sortedStk.top() <= nums[i]) {
             sortedStk.pop();
         }
@@ -24,6 +28,16 @@ vector<int> nextGreaterElement(vector<int>& nums) {
         res.push_back(sortedStk.empty()? -1 : sortedStk.top());
         sortedStk.push(nums[i]);
     }
+#if 0 
+    for (int i = 0; i < nums.size(); i++) {
+        while (!sortedStk.empty() && sortedStk.top() >= nums[i]) {
+            sortedStk.pop();
+        }
+        res.push_back(sortedStk.empty()? -1 : sortedStk.top());
+        sortedStk.push(nums[i]);
+
+    }
+#endif
 
     return res;
 }
