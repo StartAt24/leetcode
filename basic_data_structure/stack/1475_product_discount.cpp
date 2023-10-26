6b09eb9b2365db4178fe02cf9d759b1e5b719caf
StartAt24 <779058143@qq.com>
2023-10-26 23:22:04 +0800

    1475 product discount complete

--- basic_data_structure/stack/1475_product_discount.cpp
+++ basic_data_structure/stack/1475_product_discount.cpp
@@ -1,10 +1,23 @@
 #include <vector>
+#include <stack>
 using namespace std;
 
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> ret(n, 0);
+        stack<int> stk;
 
+        for(int i = n-1; i>=0; i--) {
+            while(!stk.empty() && stk.top() > prices[i])
+            {
+                stk.pop();
+            }
+            ret[i] = stk.empty()? prices[i] : prices[i] - stk.top();
+            stk.push(prices[i]);
+        }
 
+        return ret;
     }
 };
