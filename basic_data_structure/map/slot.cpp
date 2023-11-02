022fbd361541db7188b4e092c1d5c19ca15192d1
StartAt24 <779058143@qq.com>
2023-11-03 00:25:17 +0800

    slot of hashmap

--- basic_data_structure/map/slot.cpp
+++ basic_data_structure/map/slot.cpp
@@ -1,9 +1,55 @@
 #include <list>
+#include <algorithm>
 
 template<typename Key, typename Value>
 class Slot{
 public:
+    Slot() = default;
+
+    void Set(std::pair<Key, Value> c) {
+        if(!Contains(k)) {
+            _container.push_back(c);
+            _size++;
+        } else {
+            auto found =  std::find(_container.begin(), _container.end(), [](auto content) {
+                return content.first == k;
+            });
+            found->second = c.second;
+        }
+        return;
+    }
+
+    Value Get(Key k) {
+        if (!Contains(k)) {
+            throw std::exception("key not exisit");
+        }
+
+        auto found =  std::find(_container.begin(), _container.end(), [](auto content) {
+            return content.first == k;
+        });
+        return found->second;
+    }
+
+    void Delete(Key k) {
+        if (!Contains(k)) {
+            throw std::exception("key not exisit");
+        }
+
+        auto found =  std::find(_container.begin(), _container.end(), [](auto content) {
+            return content.first == k;
+        });
+        _container.erase(found);
+        _size--;
+    }
+
+    bool Contains(Key k) {
+        auto found =  std::find(_container.begin(), _container.end(), [](auto content) {
+            return content.first == k;
+        });
+        return found != _container.end();
+    }
 
 private:
     std::list<std::pair<Key, Value>> _container;
+    int _size = 0;
 };
