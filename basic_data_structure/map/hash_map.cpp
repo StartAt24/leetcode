8ac785f9f16d0ea5424e7171565c7740e9c6dae0
StartAt24 <779058143@qq.com>
2023-11-05 22:52:53 +0800

    pre commit

--- basic_data_structure/map/hash_map.cpp
+++ basic_data_structure/map/hash_map.cpp
@@ -6,8 +6,36 @@ using namespace std;
 template<typename Key, typename Value>
 class MyHashMap{
 public:
+    MyHashMap(int cap): _cap(cap) {
+        _container.reserve(cap);
+    }
+
+    ~MyHashMap() {
+
+    }
+
+    bool Contains(Key k) {
+        size_t idx = 
+    }
+
+    void Remove(Key k) {
+
+    }
+
+    void Add(key k) {
+        
+    }
+
+private:
+    size_t hash(Key k) {
+
+    }
+
+    void resize(int newCap) {
+
+    }
 
 private:
     vector<Slot<typename Key, typename Value>> _container;
-
+    int _cap{0};
 };
