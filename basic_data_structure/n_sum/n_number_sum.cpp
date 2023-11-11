a25f21c35426f7209de20d1241a74f0a8195f6e4
StartAt24 <779058143@qq.com>
2023-11-11 23:18:55 +0800

    pre commit

---
+++ basic_data_structure/n_sum/n_number_sum.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> TwoSum(vector<int> nums, int target) {
+    
+}
+
+vector<vector<int>> NSum(vector<int> nums, int target, int count) {
+    vector<vector<int>> res;
+
+
+}
