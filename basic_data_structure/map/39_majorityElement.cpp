da3e13c9b8b5ae101e0a555dc2dd1b3ab9a46c68
StartAt24 <779058143@qq.com>
2023-11-14 23:19:17 +0800

    pre commit

---
+++ basic_data_structure/map/39_majorityElement.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int target = 0;
+        int count = 0;
+        
+    }
+};
