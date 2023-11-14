aabe1392aee85538254a93c531b0edc95bd95d4e
StartAt24 <779058143@qq.com>
2023-11-14 23:28:41 +0800

    39 majority element

--- basic_data_structure/map/39_majorityElement.cpp
+++ basic_data_structure/map/39_majorityElement.cpp
@@ -6,6 +6,19 @@ public:
     int majorityElement(vector<int>& nums) {
         int target = 0;
         int count = 0;
-        
+
+        // 1,3,1,0
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                target = nums[i];
+                count = 1;
+            } 
+            else if (nums[i] == target) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return target;
     }
 };
