67e67f36f0cd8392e4bfc82a403ab25c7d956748
StartAt24 <779058143@qq.com>
2023-11-17 21:26:18 +0800

    linked hash map, a sorted map

---
+++ basic_data_structure/map/linked_hash_map.cpp
@@ -0,0 +1,30 @@
+// use a list to store all the key value pair.
+// use the actual map to store a mapping between k and pair
+// looks like use two data struture at the same time.
+
+class Map{
+public:
+    void insert(){
+
+    }
+
+    void erase(){
+
+    }
+
+    void swap(){
+
+    }
+
+    void clear() {
+
+    }
+
+    void find() {
+
+    }
+
+    int count() {
+
+    }
+};
