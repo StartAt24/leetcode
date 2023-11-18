fd20dbb064404775f52ba2ebc3d17ea133d1235a
StartAt24 <779058143@qq.com>
2023-11-18 11:14:03 +0800

    linked hash map

--- basic_data_structure/map/linked_hash_map.cpp
+++ basic_data_structure/map/linked_hash_map.cpp
@@ -1,16 +1,23 @@
 // use a list to store all the key value pair.
 // use the actual map to store a mapping between k and pair
 // looks like use two data struture at the same time.
+#include <map>
+#include <list>
 
 template<typename Key, typename Value>
-class Map{
+class MyMap{
 public:
-    void insert(){
-
+    void insert(Key k, Value v){
+        Node *n = new Node(k, v);
+        _container.insert(k, n);
+        _keyList.push_back(n);
     }
 
     void erase(Key k){
-
+        auto node = _container.find(k);
+        node->_prev->_next = node->_next;
+        node->_next->_prev = node->_prev;
+        _container.erase(k);
     }
 
     void swap(){
@@ -22,10 +29,30 @@ public:
     }
 
     Value find(Key k) {
-        throw std::exception("not found");
+        auto found = _container.find(k);
+        if(found == _container.end()) {
+            throw std::exception("not found");
+        } else {
+            return found->_v;
+        }
     }
 
     int count() {
-
+        return _keyList.size();
     }
+    struct Node{
+        Node(Key k, Value v) {
+            _k = k;
+            _v = v;
+        }
+
+        Key _k;
+        Value _v;
+        Node* _prev;
+        Node* _next;
+    };
+
+private:
+    std::unordered_map<Key, Node*> _container;
+    std::list<Node*> _keyList;
 };
