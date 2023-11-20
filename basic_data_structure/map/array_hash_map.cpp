a8055b932fd823c7b7245203fcc4d5335dd00111
StartAt24 <779058143@qq.com>
2023-11-20 23:54:20 +0800

    add container for array hash map

--- basic_data_structure/map/array_hash_map.cpp
+++ basic_data_structure/map/array_hash_map.cpp
@@ -1,11 +1,14 @@
 #include <map>
 #include <vector>
+using namespace std;
 
-// array hash map对key有 O(1)的随机访问能力
-
+// array hash map对key有 O(1)时间内,等概率的随机访问能力
+template<typename K, typename V>
 class ArrayHashMap{
 public:
 
-private:
 
+private:
+    vector<pair<K,V>> _container;
+    unordered_map<K, vector<pair<K,V>>::iterator> _map;
 };
