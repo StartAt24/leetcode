a26634419a2523eca88e1f06b10fe2918aa55fce
StartAt24 <779058143@qq.com>
2023-11-22 00:15:30 +0800

    LRU version 1.1

--- basic_algorithm/Least_recently_used.cpp
+++ basic_algorithm/Least_recently_used.cpp
@@ -16,25 +16,33 @@ public:
             // 我们就放在list的前面吧。
             // 将 found放在链表的最前面
             auto backUp = std::make_pair(found->first, found->second);
-            removeElement(found);
-            addFront(backUp.first, backUp.second);
-            return backUp.second;
+            removeElement(found->first);
+            addFront(backUp.first, backUp.second->second);
+            return backUp.second->second;
         }
         return -1;
     }
     
-    // 往末尾放
+    // 往链表头放
     void put(int key, int value) {
         if (_container.size() == _cap)
             removeLRU();
+        
+        // 如果存在的话 直接修改，并且将它提前
+        if (auto found = _map.find(key); found != _map.end()) {
+            removeElement(found->first);
+            addFront(key, value);
+            return;
+        }
 
-        _container.push_back(std::make_pair(key, value));
+        _container.push_front(std::make_pair(key, value));
     }
 
 private:
-    void removeElement(list<pair<int, int>>::iterator iter) {
+    void removeElement(int k) {
+        auto iter = _map[k];
         _container.erase(iter);
-        _map.erase(iter->first);
+        _map.erase(k);
     }
 
     void addFront(int k, int v) {
