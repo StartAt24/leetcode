af0e2482e191509778516b95f94dc121b3151ace
StartAt24 <779058143@qq.com>
2023-11-22 23:48:12 +0800

    pre commit of least frequently use

---
+++ basic_algorithm/Least_frequently_used.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+class LFUCache {
+public:
+    LFUCache(int capacity) {
+
+    }
+    
+    int get(int key) {
+
+    }
+    
+    void put(int key, int value) {
+
+    }
+private:
+    vector<int> container;
+};
