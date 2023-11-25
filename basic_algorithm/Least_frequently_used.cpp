60e5bbe6c67edd7fcd444143090c1d04181f34ec
StartAt24 <ihave6questions@gmail.com>
2023-11-25 18:33:57 +0800

    least frequent used v1.1

--- basic_algorithm/Least_frequently_used.cpp
+++ basic_algorithm/Least_frequently_used.cpp
@@ -8,16 +8,24 @@ using namespace std;
 class LFUCache {
 using iter = list<pair<int, int>>::iterator;
 public:
+    // initialize the capacity
     LFUCache(int capacity): _cap(capacity) {
     }
     
+    // get an element by key, return -1 if not exisit;
     int get(int key) {
 
+        return -1;
     }
     
     void put(int key, int value) {
-
+        //1. if exisit, change the cooresponding value and increase the number of frequency.
+        //2. if not exisit
+        //2.1 not exceed maxiumn, add it and initialize capability
+        //2.2 exceed maxium, remove the least frequence one, and add it.
+        //if there are multiple frequent elements, delete the older one.
     }
+
 private:
     list<pair<int, int>> _list;
     map<int, iter> _map;
