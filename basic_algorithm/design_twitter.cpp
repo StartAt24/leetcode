8f8b87cf7b2321a4d0b10219e207c43a25f394f3
StartAt24 <779058143@qq.com>
2023-12-03 23:37:49 +0800

    pre commit

--- basic_algorithm/design_twitter.cpp
+++ basic_algorithm/design_twitter.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <map>
 #include <list>
+#include <SET>
 using namespace std;
+#if 0
 class Twitter {
 public:
     Twitter() {
@@ -58,4 +60,79 @@ private:
     unordered_map<int, vector<int>> _followRelation;
     list<pair<int, int>> _seqTweet;
     int _maxCnt = 10;
+};
+#endif
+
+class Tweet{
+public:
+    Tweet(int id) : _id(id) {
+    }
+
+    int ID(){
+        return _id;
+    }
+
+    int Timestamp() {
+        return _timestamp;
+    }
+
+private:
+    int _timestamp{-1};
+    int _id;
+};
+
+class User{
+public:
+    User(int id) : _id(id) {
+    }
+
+    int ID() {
+        return _id;
+    }
+
+    void Post(int tweetId) {
+        _tweets.push_back(Tweet(tweetId));
+    }
+
+    const auto& GetTwitt() {
+        return _tweets;
+    }
+
+    void Follow(int user) {
+        _followees.insert(user);
+    }
+
+    void Unfollow(int user) {
+        _followees.erase(user);
+    }
+
+private:
+    set<User> _followees;
+    list<Tweet> _tweets;
+    int _id;
+};
+
+class Twitter {
+public:
+    Twitter() {
+
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        _users[userId].PostA(tweetId);
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+
+    }
+    
+    void follow(int followerId, int followeeId) {
+        _users[followerId].Follow(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        _users[followerId].Unfollow(followeeId);
+    }
+private:
+    unordered_map<int, User> _users;
 };
