b0e238c1a888c484a736e1a77fe86042e095823f
StartAt24 <779058143@qq.com>
2023-12-14 22:43:07 +0800

    reverse part of a list

--- basic_data_structure/Tree/92_reverse_part_of_list.cpp
+++ basic_data_structure/Tree/92_reverse_part_of_list.cpp
@@ -10,7 +10,42 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int begin = left;
+        while(left>=1) {
+            head = head->next;
+            left--;
+        }
 
+        ListNode* prev = head;
+        head = head->next;
+        ListNode* after = head;
+
+        ListNode* last = reverse(head, right - begin + 1);
+        prev->next = last; 
+        after->next = ;
+    }
+
+private:
+    ListNode* reverse(ListNode* head, int num) {
+        // a->b->c->d
+        // d->c->b->a
+        // a<-b<-c<-d
+        ListNode* p1 = nullptr;
+        ListNode* p2 = head;
+        ListNode* p3 = head->next;
+
+        while(num) {
+            p2->next = p1;
+            if (p3 == nullptr)
+                break;
+
+            p1 = p2;
+            p2 = p3;
+            p3 = p3->next;
+            num --;
+        }
+
+        return p2;
     }
 };
 
