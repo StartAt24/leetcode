dfbf188271b70dd096973fc61bbefcdab89c9b4a
StartAt24 <779058143@qq.com>
2023-12-25 22:55:38 +0800

    295 find median from data stream

--- basic_data_structure/Tree/295_find_median_from_data_stream.cpp
+++ basic_data_structure/Tree/295_find_median_from_data_stream.cpp
@@ -1,1 +1,29 @@
 #include <vector>
+#include <queue>
+using namespace std;
+class MedianFinder {
+public:
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+
+    }
+    
+    double findMedian() {
+        if (_minHeap.size() < _maxHeap.size()) {
+            return _maxHeap.top();
+        } else if (_minHeap.size() > _maxHeap.size()) {
+            return _minHeap.top();
+        }
+
+        return (_minHeap.top() + _maxHeap.top()) / 2;
+    }
+private:
+    
+    // 小顶堆, 实际存放的是 数值较大的那一半元素
+    priority_queue<int> _minHeap;
+    // 大顶堆，实际存放的是 数值较小的那一半元素
+    priority_queue<int> _maxHeap;
+};
