9c97e0372d903ab497a687eb0dca0420a6cefba6
StartAt24 <779058143@qq.com>
2023-12-28 23:00:45 +0800

    finish 296 find median from data stream

--- basic_data_structure/Tree/295_find_median_from_data_stream.cpp
+++ basic_data_structure/Tree/295_find_median_from_data_stream.cpp
@@ -7,9 +7,25 @@ public:
 
     }
     
+    // _minHeap的堆顶元素必须大于_maxHeap的堆顶元素
     void addNum(int num) {
-        if (!_minHeap.empty() && num > _minHeap.top()) {
+        if (_minHeap.size() >= _maxHeap.top()) {
+            // 需要插入 _maxHeap; 
 
+            // 先插入 _minHeap
+            _minHeap.push(num);
+
+            // 再将 _minHeap的堆顶插入到 _maxHeap;
+            int temp = _minHeap.top();
+            _minHeap.pop();
+            
+            _maxHeap.push(temp);
+        } else {
+            _maxHeap.push(num);
+            int temp = _maxHeap.top();
+            _maxHeap.pop();
+
+            _minHeap.push(temp);
         }
 
     }
@@ -24,7 +40,6 @@ public:
         return (_minHeap.top() + _maxHeap.top()) / 2;
     }
 private:
-    
     // 小顶堆, 实际存放的是 数值较大的那一半元素
     priority_queue<int> _minHeap;
     // 大顶堆，实际存放的是 数值较小的那一半元素
