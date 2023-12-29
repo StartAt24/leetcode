898ab842cf7251b4e9f264d89905b461ad42a8bb
StartAt24 <ihave6questions@gmail.com>
2023-12-29 22:14:25 +0800

    pre commit

---
+++ basic_data_structure/Tree/tree_map.cpp
@@ -0,0 +1,25 @@
+#include <map>
+
+struct Node {
+    Node* left;
+    Node* right;
+    int val;
+};
+
+class TreeMap {
+public:
+    TreeMap() {
+
+    }
+
+    ~TreeMap() {
+
+    }
+
+private:
+
+    void set(int k, int v) {
+
+    }
+
+};
