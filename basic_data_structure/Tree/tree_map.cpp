67515ce0dde6d68b1dba02916b3418f21e2db105
StartAt24 <ihave6questions@gmail.com>
2023-12-30 18:28:07 +0800

    tree map v1.0

--- basic_data_structure/Tree/tree_map.cpp
+++ basic_data_structure/Tree/tree_map.cpp
@@ -1,9 +1,14 @@
 #include <map>
 
 struct Node {
-    Node* left;
-    Node* right;
+    Node* left=nullptr;
+    Node* right=nullptr;
+    Node(int k, int v) : key(k), val(v) {
+
+    }
+
     int val;
+    int key;
 };
 
 class TreeMap {
@@ -16,10 +21,75 @@ public:
 
     }
 
-private:
+    void Put(int k, int v) {
+        _root = Put(k, v, _root);
+    }
 
-    void set(int k, int v) {
+    Node* Put(int k, int v, Node* node) {
+        if (node == nullptr) {
+            _size++;
+            return new Node(k, v);
+        }
+
+        if (k > node->key) {
+            node->right = Put(k, v, node->right);
+        }
+
+        if (k < node->key) {
+            node->left = Put(k, v, node->left);
+        }
+
+        if (k == node->key) {
+            node->val = v;
+        }
+
+        return node;
+    }
+
+    int Get(int k) {
+        return Get(k, _root);
+    }
+
+    int Get(int k, Node* node) {
+        // key not exist;
+        if (node == nullptr)
+            return -1;
+
+        // key exist;
+        if (node->key == k)
+            return node->val;
+        
+        // key may exist in right tree;
+        if (k > node->key) {
+            return Get(k, node->right);
+        }
+
+        // key may exisit in left tree;
+        if (k < node->key) {
+            return Get(k, node->left);
+        }
+    }
+
+    void RemoveMin() {
+        if (IsEmpty()) {
+            return;
+        }
 
     }
 
+    void RemoveMax() {
+        if (IsEmpty()) {
+            return;
+        }
+
+    }
+
+private:
+    bool IsEmpty () {
+        return _size == 0;
+    }
+
+private:
+    Node* _root = nullptr;
+    int _size = 0;
 };
