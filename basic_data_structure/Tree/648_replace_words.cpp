707c9a6b49ee0dc4c9c2c6b41d605d13cb019464
StartAt24 <779058143@qq.com>
2024-01-24 22:34:45 +0800

    648 replace words

--- basic_data_structure/Tree/648_replace_words.cpp
+++ basic_data_structure/Tree/648_replace_words.cpp
@@ -14,7 +14,44 @@ struct TrieNode{
 class TrieTree{
 public:
     void Push(string s) {
+        _root = Push(_root, s, 0);
+    }
 
+    string ShortestPrefix(string s) {
+        TrieNode* node = _root;
+        int i = 0;
+        for(auto c: s) {
+            if (!node)
+                break;
+
+            if (node->isEnd) {
+                break;
+            }
+
+            node = node->children[c];
+            i++;
+        }
+
+        if (!node)
+            return "";
+
+        return s.substr(0, i);
+    }
+
+private:
+    TrieNode* Push(TrieNode* node, string s, int i) {
+        if (!node) {
+            node = new TrieNode();
+        }
+
+        if (i == s.length()) {
+            node->isEnd = true;
+            return node;
+        }
+
+        node->children[s[i]] = Push(node->children[s[i]], s, i+1);
+
+        return node;
     }
 
 private:
@@ -24,6 +61,30 @@ private:
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
+        TrieTree t;
+        for (auto s: dictionary) {
+            t.Push(s);
+        }
 
+        string temp;
+        string result;
+        sentence.append(1, ' ');
+        for(auto c: sentence) {
+            if (c != ' '){
+                temp.append(1, c);
+            }else {
+                string prefix = t.ShortestPrefix(temp);
+                if (prefix.empty()) {
+                    result.append(temp);
+                }else {
+                    result.append(prefix);
+                }
+                result.append(1, ' ');
+                temp = "";
+            }
+        }
+        result.pop_back();
+
+        return result;
     }
 };
