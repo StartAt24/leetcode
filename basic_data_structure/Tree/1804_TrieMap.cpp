cf405a82823eac97020d9b8c04b34d0ee6e31e4d
StartAt24 <779058143@qq.com>
2024-01-26 22:56:59 +0800

    add erase function, understand erase logic

--- basic_data_structure/Tree/1804_TrieMap.cpp
+++ basic_data_structure/Tree/1804_TrieMap.cpp
@@ -22,7 +22,40 @@ public:
 
     }
 
+    bool contains(string word) {
+        TrieNode* n = getNode(_root, word);
+        return n && n->isEnd;
+    }
+
+    int count(string word) {
+        if (!contains)
+            return 0;
+
+        return getNode(_root, word)->val;
+    }
+
 private:
+    TrieNode* erase(TrieNode* node, string word, int i) {
+        if (i == word.length()) {
+            node->isEnd = false;
+        } else {
+            // need to handle it's children first, then handle itself 后续位置处理自己
+            node->children[word[i]] = erase(node->children[word[i]], word, i+1);
+        }
+
+        for (const auto& [k, v] : node->children) {
+            if (v) {
+                return node;
+            }
+        }
+        // 后序位置， 递归路径上的节点可能要被清理。
+        // 比如 一个Trie里有 ab 和 abcd, remove abcd时, c和 d都需要被清理掉 ,但是 ab不能被remove.
+        if (node->isEnd)
+            return node;
+
+        return nullptr;
+    }
+
     TrieNode* insert(TrieNode* node, string word, int i, int val) {
         if (!node)
             node = new TrieNode();
@@ -34,6 +67,17 @@ private:
         }
 
         node->children[word[i]] = insert(node->children[word[i]], word, i+1, val);
+
+        return node;
+    }
+
+    TrieNode* getNode(TrieNode* node, string word) {
+        for (auto c: word) {
+            if (!node)
+                return nullptr;
+
+            node = node->children[c];
+        }
 
         return node;
     }
