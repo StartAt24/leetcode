204ea4516c4d0b4c56df6d24410e2599ecf6c710
StartAt24 <779058143@qq.com>
2024-02-01 23:35:05 +0800

    189 rotate array

--- basic_data_structure/array/189_rotate_array.cpp
+++ basic_data_structure/array/189_rotate_array.cpp
@@ -5,6 +5,8 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        k = k%n;
+        
         // 1,2,3,4,5
         // 5,4,3,2,1
         // 4,5,1,2,3
@@ -12,13 +14,22 @@ public:
         // 1,2,3
         // 3,2,1
         // 3,1,2
-        for (int i = 0; i < n; i++) {
-            int newIdx = (i+k)%n;
-            swap(i, newIdx, nums);
-
-        }
+        revert(0, nums.size()-1, nums);
+        revert(0, k-1, nums);
+        revert(k, nums.size()-1, nums);
     }
 private:
-    void revert()
-    
+    void revert(int l, int r, vector<int>& nums) {
+        while(l < r) {
+            swap(l, r, nums);
+            l++;
+            r--;
+        }
+    }
+
+    void swap(int i, int j, vector<int>& nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 };
