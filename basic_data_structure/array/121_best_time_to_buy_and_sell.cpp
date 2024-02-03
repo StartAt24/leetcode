6cf5e6e93a366d74bf8aecee7b455b1408645639
StartAt24 <779058143@qq.com>
2024-02-03 23:51:51 +0800

    pre commit

--- basic_data_structure/array/121_best_time_to_buy_and_sell.cpp
+++ basic_data_structure/array/121_best_time_to_buy_and_sell.cpp
@@ -5,25 +5,13 @@ using std::vector;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // min stack and max stack?
-        // 7, 1, 5, 3, 6, 4
-
-        // 4
-        // 6, 4
-        // 
-        vector<int> sortedPrices;
-        for (int i = prices.size()-1; i >= 0; i--) {
-            while(!_minStack.empty() && prices[i] <= _minStack.top()) {
-                _minStack.pop();
+        int max = -1;
+        for (int i = 0; i < prices.size(); i++) {
+            for (int j = i; j< prices.size(); j++) {
+                int val = prices[j] - prices[i];
+                max = std::max(val, max);
             }
-            if (!_minStack.empty())
-                sortedPrices.push_back(_minStack.top());
-
-            _minStack.push(prices[i]);
         }
-        return sortedPrices.back()- sortedPrices.front();
+        return max;
     }
-private:
-    std::stack<int> _minStack;
-    std::stack<int> _maxStack;
 };
