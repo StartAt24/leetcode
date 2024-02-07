84f399962cff1859b63f540de870a165ba6ab7cb
StartAt24 <779058143@qq.com>
2024-02-07 22:23:11 +0800

    380 pre commit

---
+++ basic_data_structure/array/380_insert_delete_getRandom.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class RandomizedSet {
+public:
+    RandomizedSet() {
+
+    }
+    
+    bool insert(int val) {
+
+    }
+    
+    bool remove(int val) {
+
+    }
+    
+    int getRandom() {
+
+    }
+};
