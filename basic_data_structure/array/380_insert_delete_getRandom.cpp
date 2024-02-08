e562250c6db3c45afc1de33c78ca49c0ea74ed62
StartAt24 <779058143@qq.com>
2024-02-08 21:50:30 +0800

    insert delete getRandom with in O(1)

--- basic_data_structure/array/380_insert_delete_getRandom.cpp
+++ basic_data_structure/array/380_insert_delete_getRandom.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+using std::unordered_map;
 using std::vector;
 
 class RandomizedSet {
@@ -9,14 +11,36 @@ public:
     }
     
     bool insert(int val) {
-
+        if (_valToIndex.count(val))
+            return false;
+        
+        _valToIndex[val] = _nums.size();
+        _nums.push_back(val);
+        return true;
     }
     
     bool remove(int val) {
+        if (!_valToIndex.count(val))
+            return false;
+
+        int index = _valToIndex[val];
+        _valToIndex[_nums.back()] = index;
+        std::swap(_nums[index], _nums.back());
+        _nums.pop_back();
+
+        _valToIndex.erase(val);
+        return true;
 
     }
     
     int getRandom() {
-
+        return _nums[rand()%_nums.size()];
     }
+
+private:
+    // store the value of element
+    vector<int> _nums;
+    // store the index of each element
+    unordered_map<int, int> _valToIndex;
+
 };
