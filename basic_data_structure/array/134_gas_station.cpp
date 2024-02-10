15fb29b4455b7cce1a8415e185a28c7725b889e9
StartAt24 <ihave6questions@gmail.com>
2024-02-10 22:07:42 +0800

    gas status, greedy method

--- basic_data_structure/array/134_gas_station.cpp
+++ basic_data_structure/array/134_gas_station.cpp
@@ -35,3 +35,27 @@ private:
         return true;
     }
 };
+
+
+class Solution2 {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n =gas.size();
+        int sum = 0, minSum = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < minSum) {
+                start = i+1;
+                minSum = sum;
+            }
+        }
+
+        if (sum < 0) {
+            return -1;
+        }
+
+        // 环形数组， index重新校准
+        return start == n ? 0: start;
+    }
+};
