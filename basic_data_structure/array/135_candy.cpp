5c4e5fc361122246d4d0e58137c14cb875e475f7
StartAt24 <ihave6questions@gmail.com>
2024-02-11 23:14:52 +0800

    recursive method, but timeout

--- basic_data_structure/array/135_candy.cpp
+++ basic_data_structure/array/135_candy.cpp
@@ -4,6 +4,23 @@ using std::vector;
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-
+        vector<int> cadies;
+        int total = 0;
+        for (int i = 0; i < ratings.size(); i++) {
+            total += require(ratings, i);
+        }
+        return total;
+    }
+private:
+    int require(vector<int>& ratings, int idx)
+    {
+        int r = 1;
+        if (idx-1 >0 && ratings[idx-1]<ratings[idx]) {
+            r = max(r, require(ratings, idx-1)+1);
+        }
+        if (idx+1 < ratings.size() && ratings[idx+1] < ratings[idx]) {
+            r = max(r, require(ratings, idx+1) +1);
+        }
+        return r;
     }
 };
