6953862b7c55b7483ba5d5f1b00c8e77d59c7f13
StartAt24 <ihave6questions@gmail.com>
2024-02-12 23:30:22 +0800

    trapping rain water

--- basic_data_structure/array/43_trapping_rain_water.cpp
+++ basic_data_structure/array/43_trapping_rain_water.cpp
@@ -4,6 +4,20 @@ using std::vector;
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+        int left = 0, right = height.size()-1;
+        int l_max = 0, r_max = 0;
+        int res = 0;
+        while(left < right) {
+            l_max = max(l_max, height[left]);
+            r_max = max(r_max, height[right]);
+            if (l_max < r_max) {
+                res += l_max - height[left];
+                left++;
+            } else {
+                res += r_max - height[right];
+                right--;
+            }
+        }
+        return res;
     }
 };
