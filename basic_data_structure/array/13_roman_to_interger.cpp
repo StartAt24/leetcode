12162cf5e4e22ce4e6ce940d8752e0a6ecbda879
StartAt24 <ihave6questions@gmail.com>
2024-02-13 23:53:45 +0800

    roman to interger

--- basic_data_structure/array/13_roman_to_interger.cpp
+++ basic_data_structure/array/13_roman_to_interger.cpp
@@ -5,7 +5,54 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
+        int ret = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c == 'I' && i+1 < s.size()) {
+                char next = s[i+1];
+                if (next == 'V') {
+                    ret += 4;
+                    i++;
+                    continue;
+                }
+                if (next == 'X') {
+                    ret += 9;
+                    i++;
+                    continue;
+                }
+            }
 
+            if (c == 'X' && i+1 < s.size()) {
+                char next = s[i+1];
+                if (next == 'L') {
+                    ret += 40;
+                    i++;
+                    continue;
+                }
+                if (next == 'C') {
+                    ret += 90;
+                    i++;
+                    continue;
+                }
+            }
+
+            if (c == 'C' && i+1 < s.size()) {
+                char next = s[i+1];
+                if (next == 'D') {
+                    ret += 400;
+                    i++;
+                    continue;
+                }
+                if (next == 'M') {
+                    ret += 900;
+                    i++;
+                    continue;
+                }
+            }
+            ret += _char2Int[c];
+        }
+
+        return ret;
     }
 private:
     unordered_map<char, int> _char2Int {
