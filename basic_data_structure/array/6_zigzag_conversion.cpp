b31dfaa1501edc4633dc856b852da1d883128480
StartAt24 <ihave6questions@gmail.com>
2024-02-20 19:54:43 +0800

    zig zag conversion

--- basic_data_structure/array/6_zigzag_conversion.cpp
+++ basic_data_structure/array/6_zigzag_conversion.cpp
@@ -1,9 +1,29 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     string convert(string s, int numRows) {
+        string ret;
 
+        vector<string> rows;
+        rows.resize(numRows);
+        int i = 0;
+        int flag = -1;
+
+        for (auto c : s) {
+            rows[i].push_back(c);
+            if (i == 0 || i == numRows-1) {
+                flag = -flag;
+            }
+            i+=flag;
+
+        }
+        for (auto str : rows ) {
+            ret += str;
+        }
+
+        return ret;
     }
 };
