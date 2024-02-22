f5ed284e9978e03b906f65a94ca4f3935346ca61
StartAt24 <779058143@qq.com>
2024-02-22 23:32:56 +0800

    pre commit

--- basic_data_structure/array/28_find_the_index_of_the_first_occurrence.cpp
+++ basic_data_structure/array/28_find_the_index_of_the_first_occurrence.cpp
@@ -3,6 +3,13 @@ using std::string;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-
+        char begin = haystack[0];
+        int ret = -1;
+        for (int i = 0; i < needle.size(); i++) {
+            if (needle[i] == begin) {
+                
+            }
+        }
+        return ret;
     }
 };
