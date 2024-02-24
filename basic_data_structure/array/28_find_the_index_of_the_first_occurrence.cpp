0c72f4efb2f277de4c090bab25f6546f4ea34346
StartAt24 <779058143@qq.com>
2024-02-24 23:31:06 +0800

    interesting KMP algorithm

--- basic_data_structure/array/28_find_the_index_of_the_first_occurrence.cpp
+++ basic_data_structure/array/28_find_the_index_of_the_first_occurrence.cpp
@@ -1,15 +1,37 @@
+#include <vector>
 #include <string>
+#include <algorithm>
+
 using std::string;
+using std::vector;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        char begin = haystack[0];
-        int ret = -1;
-        for (int i = 0; i < needle.size(); i++) {
-            if (needle[i] == begin) {
-                
+        // KMP algorithm
+        // 1. create a partial match table of each index in the string.
+        // abcd
+        // prefix string: a, ab, abc
+        // suffix string: bcd, cd, d
+        // the num of this string is the size of the longest overlap string.
+        // here it's 0.
+
+
+    }
+private:
+    int KMP(string target, string pattern) {
+        int i = 0;
+        int j = 0;
+        while (i < target.size() && j < pattern.size()) {
+            // if J equals to -1, it means it need to be rest
+            // and the comparasion should start with the next new index;
+            if (j == -1 || target[i] == pattern[j]) {
+                i++;
+                j++;
+            } else {
+                j = _next[j];
             }
         }
-        return ret;
     }
+
+    vector<int> _next;
 };
