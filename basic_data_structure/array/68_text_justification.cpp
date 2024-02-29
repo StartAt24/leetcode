4f9594121baa59f80d7392df8fba14f26ecf369d
StartAt24 <ihave6questions@gmail.com>
2024-02-29 22:51:29 +0800

    pre commit

--- basic_data_structure/array/68_text_justification.cpp
+++ basic_data_structure/array/68_text_justification.cpp
@@ -1,6 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        
 
     }
 };
---
+++ classic_150/slide_window/209_minimum_size_subarray_sum.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+    }
+};
