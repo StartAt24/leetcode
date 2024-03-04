67278b0f97f90480d7054e384628c0350534ad11
StartAt24 <ihave6questions@gmail.com>
2024-03-05 07:16:26 +0800

    11 container with most water

--- classic_150/double_pointer/11_container_with_most_water.cpp
+++ classic_150/double_pointer/11_container_with_most_water.cpp
@@ -1,6 +1,20 @@
+#include <vector>
+using std::vector;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-
+        int area = 0, res = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            area = (right - left) * min(height[left], height[right]);
+            res = max(res, area);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
     }
 };
