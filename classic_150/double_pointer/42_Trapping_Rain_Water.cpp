e676ac11f83636397753cad4e4db82134bb9c866
StartAt24 <779058143@qq.com>
2024-03-06 23:45:30 +0800

    42 trapping rain water

--- classic_150/double_pointer/42_Trapping_Rain_Water.cpp
+++ classic_150/double_pointer/42_Trapping_Rain_Water.cpp
@@ -3,6 +3,22 @@ using std::vector;
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int left = 0, right = height.size()-1;
+        int maxLeft = 0, maxRight = 0;
+        int ret = 0;
+        while(left < right) {
+            maxLeft = max(maxLeft, height[left]);
+            maxRight = max(maxRight, height[right]);
 
+            // left is a valid place
+            if (maxLeft < maxRight) {
+                ret += (maxLeft - height[left]);
+                left++;
+            } else {
+                ret += (maxRight - height[right]);
+                right--;
+            }
+        }
+        return ret;
     }
 };
