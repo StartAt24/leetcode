f4032f6a94d459cdfb16f09c41f7dbc8069dce00
StartAt24 <779058143@qq.com>
2024-03-09 23:45:06 +0800

    pre commit

--- classic_150/slide_window/209_minimum_size_subarray_sum.cpp
+++ classic_150/slide_window/209_minimum_size_subarray_sum.cpp
@@ -4,6 +4,11 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0, right = 0;
+        vector<int> window;
+        
+        while (right < nums.size()) {
 
+        }
     }
 };
