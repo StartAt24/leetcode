c492de789da85f4aefc0935c7ff14577a8d3e2da
StartAt24 <779058143@qq.com>
2024-03-10 10:17:59 +0800

    209 minimun size subarray

--- classic_150/slide_window/209_minimum_size_subarray_sum.cpp
+++ classic_150/slide_window/209_minimum_size_subarray_sum.cpp
@@ -1,14 +1,53 @@
 #include <vector>
 using namespace std;
 
+class Window{
+public:
+    void push(int n) {
+        _max += n;
+        _size++;
+    }
+
+    int sum() {
+        return _max;        
+    }
+
+    void pop(int n) {
+        _max -= n;
+        _size--;
+    }
+
+    int size() {
+        return _size;
+    }
+
+private:
+    int _size = 0;
+    int _max = 0;     
+};
+
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int left = 0, right = 0;
-        vector<int> window;
-        
-        while (right < nums.size()) {
+        int minSize = nums.size()+1;
 
+        while (right < nums.size()) {
+            _win.push(nums[right]);
+
+            while(_win.sum() >= target) {
+                minSize = min(_win.size(), minSize);
+                _win.pop(nums[left]);
+                left++;
+            }
+            right++;
         }
+
+        if (minSize == nums.size()+1)
+            return 0;
+
+        return minSize;
     }
+private:
+    Window _win; 
 };
