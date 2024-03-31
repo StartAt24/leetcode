c3d2b291f767f7d4098189a8da83951b8928d490
StartAt24 <ihave6questions@gmail.com>
2024-03-31 20:18:41 +0800

    construct a tree with inorder and postorder

--- classic_150/binary_tree/106_construct_binary_tree_from_inorder_and_postorder_traversal.cpp
+++ classic_150/binary_tree/106_construct_binary_tree_from_inorder_and_postorder_traversal.cpp
@@ -1,5 +1,7 @@
 #include <vector>
+#include <map>
 using std::vector;
+using std::unordered_map;
 struct TreeNode {
      int val;
      TreeNode *left;
@@ -12,13 +14,36 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
+        for(int i = 0; i < inorder.size(); i++) {
+            _value2Index[inorder[i]] = i;
+        }
+        TreeNode* root = build(inorder, 0, inorder.size()-1, postorder, 0, postorder.size()-1);
+        return root;
     }
 private:
-    // inorder:     [left_child] root [right_child]
-    // postorder:   [left_child] [right_cihld] [root]
+    //           inStart    left_size           idx              inEnd
+    // inorder:     [       left_child      ]   root  [right_child]
+    // 
+    //          postStart                                       postEnd
+    // postorder:   [       left_child      ] [right_cihld] [root]
+    // 
+
+
     TreeNode* build(vector<int>& inorder, int inStart, int inEnd,
                     vector<int>& postorder, int postStart, int postEnd) {
+        // cannot find root value
+        if (postStart > postEnd)
+            return nullptr;
 
+        int currentValue = postorder[postEnd];
+        TreeNode* n = new TreeNode(currentValue);
+        int idx = _value2Index[currentValue];
+        int left_size = idx - inStart;
+
+        n->left = build(inorder, inStart, idx-1, postorder, postStart, postStart+left_size-1);
+        n->right = build(inorder, idx+1, inEnd, postorder, postStart + left_size, postEnd-1);
+        return n;
     }
+
+    unordered_map<int, int> _value2Index;
 };
