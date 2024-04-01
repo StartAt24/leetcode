30c2318769cc31f6af3f51a4bfc29e036efd357b
StartAt24 <ihave6questions@gmail.com>
2024-04-01 23:35:23 +0800

    build tree from pre and post order

--- classic_150/binary_tree/889_construct_binary_tree_from_postorder_and_preorder.cpp
+++ classic_150/binary_tree/889_construct_binary_tree_from_postorder_and_preorder.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <map>
+using std::unordered_map;
 using std::vector;
 
 //  Definition for a binary tree node.
@@ -14,26 +16,43 @@ using std::vector;
 class Solution {
 public:
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-
+        for (int i = 0; i < postorder.size(); i++) {
+            int val = postorder[i];
+            _indexOfValue[val] = i;
+        }
     }
 private:
+    unordered_map<int, int> _indexOfValue;
     TreeNode* build(const vector<int>& preorder, int preStart, int preEnd, 
                     const vector<int>& postorder, int postStart, int postEnd){
         // preorder
-        // [root][left_child] [right_child]
+        // preStart   leftRootValue              preStart+ left_size            preEnd
+        // [   root     ][           left_child        ][              right_child]
         // postorder:
-        // [left_child][right_child][root]
+        // postStart                    leftRootValue
+        //                                 index                                   postEnd
+        // [            left_child          ][         right_child             ][root]
         
         // basic part, when to stop?
         if (preStart > preEnd || postStart > postEnd)
             return nullptr;
 
+        // 这个时候就不能再往后吗走了，因为后面的涉及到+1的操作
+        if (preStart == preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
 
-        int rootVal = preorder[0];
+        int rootVal = preorder[preStart];
         TreeNode* root = new TreeNode(rootVal);
 
-        root->left = build();
-        root->right = build();
+        int leftRootVal = preorder[preStart+1];
+        int idx = _indexOfValue[leftRootVal];
+        int leftSize = idx - postStart + 1;
+
+
+        root->left = build(preorder, preStart+1, preStart+leftSize, postorder, postStart, index);
+        root->right = build(preorder, preStart+leftSize+1, preEnd, postorder, index+1, postEnd-1);
+
 
         return root;
     }
