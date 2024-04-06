68da9fd5b5eb789cc9a0ea9720c9a4ca41d618a6
StartAt24 <ihave6questions@gmail.com>
2024-04-07 00:10:47 +0800

    merge sort

--- classic_150/binary_tree/merge_sort.cpp
+++ classic_150/binary_tree/merge_sort.cpp
@@ -3,28 +3,43 @@
 using namespace std;
 
 void Merge(vector<int>& arr, int lo, int mid, int hi) {
-    int p1 = lo;
-    int p2 = mid+1;
     // 1,3, 2,4
     // p1,  p2
-    
+    int size = hi-lo+1;
+    vector<int> temp(size);
 
+    int p1 = lo;
+    int p2 = mid+1;
 
-    while(1) {
-        if (arr[p1] <= arr[p2]) {
-            p1++;
+    for (int i = 0; i < size; i++) {
+        // 这里不能单纯的用双指针去遍历两者，要考虑到其中一个遍历结束的情况
+        // 左侧已经
+        if (p1 == mid+1) {
+            temp[i] = arr[p2++];
+        } else if (p2 == hi) {
+            temp[i] = arr[p1++];
+        } else if (arr[p1] < arr[p2]) {
+            temp[i] = arr[p1++];
         } else {
-            arr[p1] = arr[p2];
-            p2++;
+            temp[i] = arr[p2++];
         }
-
-
     }
 
+    std::copy(temp.begin(), temp.end(), arr.begin()+lo);
     return;
 }
 
 void MergeSort(vector<int>& arr) {
+    MergeSort(arr, 0, arr.size()-1);
+}
 
+void MergeSort(vector<int>& arr, int lo, int hi) {
+    if(lo == hi)
+        return;
+    int mid = lo + (hi-lo)/2;
+    MergeSort(arr, lo, mid);
+    MergeSort(arr, mid+1, hi);
+
+    Merge(arr, lo, mid, hi);
 }
 
