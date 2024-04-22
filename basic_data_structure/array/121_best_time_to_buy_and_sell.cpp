a908008f8bc684bacf0f76465356441fb23f8d06
StartAt24 <ihave6questions@gmail.com>
2024-04-22 21:41:39 +0800

    DP is hard to understand

--- basic_data_structure/array/121_best_time_to_buy_and_sell.cpp
+++ basic_data_structure/array/121_best_time_to_buy_and_sell.cpp
@@ -6,7 +6,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int max = -1;
+        int pre = prices[0];
         for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] >= pre)
+                continue;
+            else {
+                pre = prices[i];
+            }
+
             for (int j = i; j< prices.size(); j++) {
                 int val = prices[j] - prices[i];
                 max = std::max(val, max);
