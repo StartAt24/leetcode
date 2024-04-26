e28692dd43ee0dd644d607d38238b8aa21bd5540
StartAt24 <ihave6questions@gmail.com>
2024-04-27 00:29:35 +0800

    priority queue

--- basic_data_structure/binary_search/215_kth_largest_element_in_an_array.cpp
+++ basic_data_structure/binary_search/215_kth_largest_element_in_an_array.cpp
@@ -1,8 +1,18 @@
 #include <vector>
+#include <queue>
 using std::vector;
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
+        std::priority_queue<int ,vector<int>, std::less<int>> maxHeap;
+        for(auto n : nums) {
+            maxHeap.insert(n);
+        }
+
+        while (k--) {
+            maxHeap.pop();
+        }
+
+        return maxHeap.top() 
     }
-};
+};      
