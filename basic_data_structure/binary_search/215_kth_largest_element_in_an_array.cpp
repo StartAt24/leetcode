238dfdd69790b9892630236796a307b65917ed9e
StartAt24 <ihave6questions@gmail.com>
2024-04-28 01:18:07 +0800

    use quick sort to solve find Kth

--- basic_data_structure/binary_search/215_kth_largest_element_in_an_array.cpp
+++ basic_data_structure/binary_search/215_kth_largest_element_in_an_array.cpp
@@ -15,4 +15,51 @@ public:
 
         return maxHeap.top() 
     }
-};      
+};
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        _targetPisition = nums.size() - k;
+        sort(nums, 0, nums.size()-1);
+        return _res;
+    }
+
+private:
+    void sort(vector<int>& nums, int lo, int hi) {
+        if (lo>=hi)
+            return;
+        // 将选择的pivot排序到正确的位置
+        int p = partition(nums, lo, hi);
+        if (p < _targetPisition) {
+            sort(nums, p+1, hi);
+        } else {
+            sort(nums, lo, p-1);
+        } else {
+            _res =  nums[p];
+            return;
+        }
+    }
+
+    int partition(vector<int>& nums, int lo, int hi) {
+        int pivot = nums[lo];
+        int left = lo+1;
+        int right = hi;
+        while (1) {
+            while (left <= right && nums[left] <= pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+
+            if (left >= right)
+                break;
+            swap(nums[left], nums[right]);
+        }
+        swap(nums[lo], mums[right]);
+        return right;
+    }
+    
+    int _targetPisition = 0;
+    int _res = 0;
+};
