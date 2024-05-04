dbddb655c5476e4b2135ab1255de65d8cf8ab84a
StartAt24 <ihave6questions@gmail.com>
2024-05-04 21:37:17 +0800

    maximum width of binary tree

--- basic_data_structure/binary_tree_exercise/662_maximum_width_of_binary_tree.cpp
+++ basic_data_structure/binary_tree_exercise/662_maximum_width_of_binary_tree.cpp
@@ -33,5 +33,23 @@ private:
         }
     }
 
+    // 将当前的节点的idx，从父节点传递下去，以及depth
+    void traverse2(TreeNode* n, int id, int depth) {
+        if (!n)
+            return;
+        // 这里是最左侧的节点，并且因为是先左后右的递归顺序，所以 必定是左侧第一个节点先进去的
+        if (_firstId.size() == depth-1) {
+            firstId.push_back(id);
+        } else {
+            _maxWidth = max(_maxWidth, id-_firstId[depth] + 1);
+        }
+
+        traverse2(root->left, id*2, depth+1);
+        traverse2(root->right, id*2 + 1, depth+1);
+    }
+    int _maxWidth = 0;
+    // 记录最左侧节点的编号
+    vector<int> _firstId;
+
     int _res{0};
 };
