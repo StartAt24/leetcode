e5282d71a2cbc5596bf4ca678083aee3a976ebab
StartAt24 <ihave6questions@gmail.com>
2024-05-07 23:54:43 +0800

    very interesting pre in post order

--- basic_data_structure/binary_tree_exercise/270_nearest_value_in_bst.cpp
+++ basic_data_structure/binary_tree_exercise/270_nearest_value_in_bst.cpp
@@ -21,3 +21,24 @@ class Solution{
 
     int _res = INT_MAX;
 };
+
+// 只要是从小到大的顺序遍历，那么第一个更新的就是比较小的那个值
+void traverse(TreeNode* n, int target) {
+    if (!n)
+        return;
+    
+    if (n->val > target) {
+        traverse(n->left, target);
+        // 这个位置是 中序遍历的位置
+        if(abs(n->val - target) < abs(_res - target)) {
+            _res = n->val;
+        }
+
+    } else {
+        // 这个位置， 也是中序位置
+        if(abs(n->val - target) < abs(_res - target)) {
+            _res = n->val;
+        }
+        traverse(n->right, target);
+    }
+}
