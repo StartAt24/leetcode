eb7b335ac6d17e34f9468ea7574fc4d041533f07
StartAt24 <ihave6questions@gmail.com>
2024-05-10 23:12:40 +0800

    971 flip binary tree

--- basic_data_structure/binary_tree_exercise/971_flip_binary_tree_to_match_preorder_traversal.cpp
+++ basic_data_structure/binary_tree_exercise/971_flip_binary_tree_to_match_preorder_traversal.cpp
@@ -4,21 +4,35 @@ class Solution {
 public:
     vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
         _voyage = voyage;
+        traverse(root);
+        if(_match) {
+            return _res;
+        } else {
+            return {-1};
+        }
     }
 private:
     void traverse(TreeNode* n) {
         if (!n)
             return;
         
-        if (n->val != _vogage[_idx]){
+        if (n->val != _voyage[_idx]){
             _match = false;
             return;
         }
 
-        if (n->left && )
-        
+        _idx++;
+        if (n->left && n->left->val != _voyage[_idx]) {
+            auto temp = n->right;
+            n->right = n->left;
+            n->left = temp;
+            _res.push_back(n->val);
+        }
+        traverse(n->left);
+        traverse(n->right);
     }
     bool _match = true;
     int _idx = 0;
+    vector<int> _res;
     vector<int> _voyage;
 };
