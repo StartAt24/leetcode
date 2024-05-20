51764c9874a42a315ee2718bb92eaf2fdc6529a5
StartAt24 <ihave6questions@gmail.com>
2024-05-20 23:27:02 +0800

    another tree

--- basic_data_structure/binary_tree_exercise/572_subtree_of_another_tree.cpp
+++ basic_data_structure/binary_tree_exercise/572_subtree_of_another_tree.cpp
@@ -3,18 +3,32 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-
+        if (!subRoot)
+            return false;
+        _subRoot = subRoot;
+        return traverse(root);
     }
 
 private:
+    TreeNode* _subRoot = nullptr;
+
+    bool traverse(TreeNode* node) {
+        if (!node)
+            return false;
+
+        if (node->val == _subRoot->val) {
+            if(traverse(node, _subRoot))
+                return true;
+        }
+
+        return traverse(node->left) || traverse(node->right);
+    }
+
     bool traverse(TreeNode* n1, TreeNode* n2) {
         if (!n1 && !n2)
             return true;
 
-        if (n1 && !n2)
-            return false;
-
-        if (!n1 && n2)
+        if (!n2 || !n1)
             return false;
         
         if (n1->val != n2->val)
