c222cd0388516c040cc5d841fc171ef69dd63755
StartAt24 <ihave6questions@gmail.com>
2024-05-31 06:59:51 +0800

    prior to interview

---
+++ classic_150/slide_window/framework.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int slide(vector<string> s) {
+    int slow = 0, fast = 0;
+    vector<string> window;
+    while(slow < fast) {
+        window.push_back(s[fast]);
+        fast++;
+        while(xxx) {
+            windows.pop_front();
+            slow++;
+        }
+    }
+
+}
